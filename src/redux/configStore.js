995b425b10823f5de17ff5fc29e66e17e03df85d
tuankiet02 <luongtuankiet210102@gmail.com>
2025-12-30 17:03:12 +0700

    change some texts

--- src/redux/configStore.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import thongTinDatVeReduxcer from "./thongTinDatVeReduxcer";
-
-export const store = configureStore({
-  reducer: {
-    thongTinDatVeReduxcer: thongTinDatVeReduxcer,
-  },
-});
